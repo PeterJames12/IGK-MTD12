9c8958ce5a7a4d6497ccb6f94c4e0a5195d725e8
Andre <andre.viyt@gmail.com>
2017-01-31 19:49:18 +0200

    remove lombok

--- src/main/java/model/entity/Developer.java
+++ src/main/java/model/entity/Developer.java
@@ -1,13 +1,8 @@
 package model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author devcolibri on 20.01.17.
  */
-@Setter
-@Getter
 public class Developer {
 
     private String name;
@@ -15,5 +10,35 @@ public class Developer {
     private String skills;
 
     public Developer() {
+    }
+
+    public Developer(String name, String email, String skills) {
+        this.name = name;
+        this.email = email;
+        this.skills = skills;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
     }
 }
--- src/main/java/model/entity/Project.java
+++ src/main/java/model/entity/Project.java
@@ -1,15 +1,9 @@
 package model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author Igor Gnes on 05.01.17.
  */
-@Setter
-@Getter
-@AllArgsConstructor
+
 public class Project {
 
     private int id;
@@ -18,5 +12,44 @@ public class Project {
     private String task;
 
     public Project() {
+    }
+
+    public Project(int id, String name, String description, String task) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.task = task;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
     }
 }
